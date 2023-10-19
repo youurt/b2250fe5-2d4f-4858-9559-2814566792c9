c15b9f05d3c47833910fdcbcc58a7124ad929424
youurt <ugurtigu@gmail.com>
2023-10-19 19:14:21 +0200

    [general-app-boilerplate] * remove static import

--- apps/eventify-frontend/src/app/app.component.ts
+++ apps/eventify-frontend/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { EventsComponent } from '@eventify-org/events';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, EventsComponent],
+  imports: [RouterModule],
   selector: 'eventify-org-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
