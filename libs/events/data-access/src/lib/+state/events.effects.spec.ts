207d04a90575ac1f4005c88442165244055e389f
youurt <ugurtigu@gmail.com>
2023-10-20 19:21:47 +0200

    - add test for success

--- libs/events/data-access/src/lib/+state/events.effects.spec.ts
+++ libs/events/data-access/src/lib/+state/events.effects.spec.ts
@@ -1,16 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { createEvent } from '@eventify-org/common-api';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Action } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
 import { hot } from 'jasmine-marbles';
-import { Observable } from 'rxjs';
-
+import { Observable, of } from 'rxjs';
+import { EventsHttpService } from '../services/events-http.service';
 import * as EventsActions from './events.actions';
 import { EventsEffects } from './events.effects';
 
 describe('EventsEffects', () => {
   let actions: Observable<Action>;
   let effects: EventsEffects;
+  let service: EventsHttpService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -19,21 +21,33 @@ describe('EventsEffects', () => {
         EventsEffects,
         provideMockActions(() => actions),
         provideMockStore(),
-      ],
+        {
+          provide: EventsHttpService,
+          useValue: <Partial<EventsHttpService>>{
+            getEventifyEvents: jest.fn()
+          }
+        }
+      ]
     });
 
     effects = TestBed.inject(EventsEffects);
+    service = TestBed.inject(EventsHttpService);
   });
 
-  describe('init$', () => {
-    it('should work', () => {
-      actions = hot('-a-|', { a: EventsActions.initEvents() });
+  describe('loadEvents$', () => {
+    it('success', () => {
+      const eventifyEvents = [createEvent(), createEvent({ title: 'test' })];
+
+      jest.spyOn(service, 'getEventifyEvents').mockReturnValue(of(eventifyEvents));
+
+      actions = hot('-a-|', { a: EventsActions.loadEvents() });
 
       const expected = hot('-a-|', {
-        a: EventsActions.loadEventsSuccess({ events: [] }),
+        a: EventsActions.loadEventsSuccess({ eventifyEvents })
       });
 
-      expect(effects.init$).toBeObservable(expected);
+      expect(effects.loadEvents$).toBeObservable(expected);
+      expect(service.getEventifyEvents).toHaveBeenCalled();
     });
   });
 });
