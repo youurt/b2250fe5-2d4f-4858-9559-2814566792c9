ae9e2ac96758b71eceb59f9c88a87749e3ab3517
youurt <ugurtigu@gmail.com>
2023-11-10 16:16:19 +0100

    fix venue

--- libs/events/data-access/src/lib/services/events-http.service.ts
+++ libs/events/data-access/src/lib/services/events-http.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { EventifyEvent } from '@eventify-org/common-api';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class EventsHttpService {
@@ -12,6 +12,16 @@ export class EventsHttpService {
    * @returns An `Observable<EventifyEvent[]>`.
    */
   getEventifyEvents(): Observable<EventifyEvent[]> {
-    return this.httpClient.get<EventifyEvent[]>(this.apiUrl);
+    return this.httpClient.get<EventifyEvent[]>(this.apiUrl).pipe(
+      map(eventifyEvents =>
+        eventifyEvents.map(eventifyEvent => ({
+          ...eventifyEvent,
+          venue: {
+            ...eventifyEvent.venue,
+            direction: eventifyEvent.venue.direction.replace('/Colour+Factory+london', eventifyEvent.venue.name)
+          }
+        }))
+      )
+    );
   }
 }
